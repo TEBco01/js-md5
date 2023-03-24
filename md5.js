be0c3c506e3415373631f4516a18c9bb1cf7c954
Tyler <TEBcoyo1@gmail.com>
2023-03-24 17:14:50 -0500

    Add 64-bit length counting

--- md5.js
+++ md5.js
@@ -15,15 +15,15 @@ module.exports = {
 function md5_init() {
   var ctx = {
     'state' : [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476],
-    'count' : 0, // In bytes
+    'count' : new Uint32Array([0,0]), // In bytes
     'buffer' : new Uint8Array(64)
   };
 
   return ctx;
 }
 
-function get_padding(len_bytes) {
-  mod_len_bytes = len_bytes % 64;
+function get_padding(len_bits) {
+  mod_len_bytes = (len_bits >>> 3) % 64;
   num_bytes = 64;
   if (mod_len_bytes != 56) {
     num_bytes = (64 + 56 - mod_len_bytes) % 64;
@@ -44,14 +44,14 @@ function H(x, y, z) {return ((x) ^ (y) ^ (z));}
 function I(x, y, z) {return ((y) ^ ((x) | (~z)));}
 
 function get_length_bits(len_in_bits) {
-  return [len_in_bits & 0xFF,
-    (len_in_bits >>> 8) & 0xFF,
-    (len_in_bits >>> 16) & 0xFF,
-    (len_in_bits >>> 24) & 0xFF,
-    0,//(len_in_bytes >>> 32) & 0xFF,  // Only counting up to 32 bits for now; 64 TBI
-    0,//(len_in_bytes >>> 40) & 0xFF,
-    0,//(len_in_bytes >>> 48) & 0xFF,
-    0];//(len_in_bytes >>> 56) & 0xFF];
+  return [len_in_bits[0] & 0xFF,
+    (len_in_bits[0] >>> 8) & 0xFF,
+    (len_in_bits[0] >>> 16) & 0xFF,
+    (len_in_bits[0] >>> 24) & 0xFF,
+    (len_in_bits[1] >>> 32) & 0xFF,
+    (len_in_bits[1] >>> 40) & 0xFF,
+    (len_in_bits[1] >>> 48) & 0xFF,
+    (len_in_bits[1] >>> 56) & 0xFF];
 }
 
 function rotate_left(value, n) {
@@ -150,8 +150,15 @@ function md5_transform(ctx, buf) {
 }
 
 function md5_update(buf, ctx) {
-  index = ctx.count % 64;
-  ctx.count += buf.length;
+  index = (ctx.count[0] >> 3) & 0x3F;
+
+  ctx.count[0] += buf.length << 3;
+  ctx.count[0] &= 0xFFFFFFFF;
+  if(ctx.count[0] < (buf.length << 3)) {
+    ctx.count[1]++;
+  }
+  ctx.count[1] += buf.length >>> 29;
+
   partLen = 64 - index;
   let i = 0;
 
@@ -175,8 +182,8 @@ function md5_update(buf, ctx) {
 }
 
 function md5_final(ctx) {
-  padding = get_padding(ctx.count);
-  length = get_length_bits(ctx.count*8);
+  padding = get_padding(ctx.count[0]);
+  length = get_length_bits(ctx.count);
   md5_update(padding, ctx);
   md5_update(length, ctx);
 
